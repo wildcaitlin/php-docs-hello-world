4766b339c7c50eaa193eb9b207fec23d0e9f2e7a
Caitlin <notcaitlinw@gmail.com>
2024-10-16 09:31:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 header('Content-Type: application/json');
+
 $data = array("key1" => "value1", "key2" => "value2");
 echo json_encode($data);
 ?>
